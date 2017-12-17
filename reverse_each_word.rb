6d4d03260f229ea81e3d5b40c1a7b77d8711fe34
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-17 00:14:05 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,6 @@ def reverse_each_word(string)
   words.each do |word|
     my_statements << word.reverse
   end
-  product = my_statements.join
+  product = my_statements.join(" ")
   return product
 end
